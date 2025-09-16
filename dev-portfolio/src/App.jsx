728e52f2cc2e51671734ac08077c140234271faa
Rommel Agudo <rommelrian17@gmail.com>
2025-09-16 13:05:59 +1200

    fix/modified-hero-title-and-description

--- dev-portfolio/src/App.jsx
+++ dev-portfolio/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { ThemeProvider } from "./context/ThemeContext";
 import Navbar from "./components/Sections/Navbar";
+import HeroSection from "./components/Sections/HeroSection";
 
 const App = () => {
   return (
     <ThemeProvider>
     <div>
       <Navbar />
+      <HeroSection />
     </div>
     </ThemeProvider>
   );
